3cba9af0bdaa609c5f2e42ed65683cbffe29ed95
samuelcoates <samuelcoates@students.berkeley.net>
2017-03-03 11:10:39 -0800

    Update music.js

--- music.js
+++ music.js
@@ -1,26 +1,52 @@
-/**
- * Created by h205p3 on 2/13/17.
- */
-function getArtist() {
+function getId() {
     var artist = document.getElementById("inputArtist").value;
     console.log(artist);
-    url = "http://www.songsterr.com/a/ra/songs/byartists.json?artists=";
-    url += '?' + $.param({
-            'a': artist
+    var url = "http://www.songsterr.com/a/ra/songs/byartists.json?";
+    url += $.param({
+            'artists': artist
         });
-    url += '&' + $.param({
+console.log (url);
+    $.ajax({
+    //    url: "http://www.songsterr.com/a/ra/songs/byartists.json?artists=",
+        url: url,
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'json',
+        success: function(result) { console.log(result); handler(result);},
+        error: function() { alert('Failed!'); }
+    });
 
-            'a': artist
-        });
+
 }
 
-$.ajax({
-    url: "http://www.songsterr.com/a/ra/songs/byartists.json?artists=",
-    type: 'GET',
-    crossDomain: true,
-    dataType: 'json',
-    success: function(result) { console.log(result) },
-    error: function() { alert('Failed!'); }
-});
+
+function handler(result) {
+
+    for(var i=0;i<result.length;i++) {
+        $("#songs").append("<tr><td><a href='http://www.songsterr.com/a/wa/song?id='>" + result[i].title + "</a></td><td>" + result[i].id  + "</td></tr>");
+    }
+
+}
 
 
+/*
+function getSong() {
+    var song = document.getElementById("inputSong").value;
+    console.log(song);
+    var url = "http://www.songsterr.com/a/ra/songs/bysongs.json?";
+    url += $.param({
+        'songs': song
+    });
+    console.log (url);
+    $.ajax({
+        //    url: "http://www.songsterr.com/a/ra/songs/byartists.json?artists=",
+        url: url,
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'json',
+        success: function(result) { console.log(result); },
+        error: function() { alert('Failed!'); }
+    });
+
+}
+    */
