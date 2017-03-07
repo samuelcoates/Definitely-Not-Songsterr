db7491f49b95ac808d881108d487cbcc7f516252
samuelcoates <samuelcoates@students.berkeley.net>
2017-03-07 11:24:48 -0800

    Update music.js

--- music.js
+++ music.js
@@ -1,3 +1,6 @@
+/**
+ * Created by h205p3 on 2/13/17.
+ */
 function getId() {
     var artist = document.getElementById("inputArtist").value;
     console.log(artist);
@@ -21,13 +24,18 @@ console.log (url);
 
 
 function handler(result) {
-
+    $("#songs").empty();
     for(var i=0;i<result.length;i++) {
         $("#songs").append("<tr><td><a href='http://www.songsterr.com/a/wa/song?id="+ result[i].id + "'>" + result[i].title + "</a></td><td>" + result[i].id  + "</td></tr>");
     }
 
 }
 
+function removeTables() {
+    var tables = document.getElementsByTagName("TABLE");
+    for (var i=tables.length-1; i>=0;i-=1)
+        if (tables[i]) tables[i].parentNode.removeChild(tables[i]);
+}
 
 /*
 function getSong() {
