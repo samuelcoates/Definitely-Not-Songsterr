c741026ee2b95b850674e53985ee0b8c7abe133f
samuelcoates <samuelcoates@students.berkeley.net>
2017-03-22 10:47:58 -0700

    Update music.js

--- music.js
+++ music.js
@@ -6,17 +6,22 @@ function getId() {
     console.log(artist);
     var url = "http://www.songsterr.com/a/ra/songs/byartists.json?";
     url += $.param({
-            'artists': artist
-        });
-console.log (url);
+        'artists': artist
+    });
+    console.log(url);
     $.ajax({
-    //    url: "http://www.songsterr.com/a/ra/songs/byartists.json?artists=",
+        //    url: "http://www.songsterr.com/a/ra/songs/byartists.json?artists=",
         url: url,
         type: 'GET',
         crossDomain: true,
-        dataType: 'json',
-        success: function(result) { console.log(result); handler(result);},
-        error: function() { alert('Failed!'); }
+        dataType: 'jsonp',
+        success: function(result) {
+            console.log(result);
+            handler(result);
+        },
+        error: function() {
+            alert('Failed!');
+        }
     });
 
 
@@ -25,36 +30,14 @@ console.log (url);
 
 function handler(result) {
     $("#songs").empty();
-    for(var i=0;i<result.length;i++) {
-        $("#songs").append("<tr><td><a href='http://www.songsterr.com/a/wa/song?id="+ result[i].id + "'>" + result[i].title + "</a></td><td>" + result[i].id  + "</td></tr>");
+    for (var i = 0; i < result.length; i++) {
+        $("#songs").append("<tr><td><a href='http://www.songsterr.com/a/wa/song?id=" + result[i].id + "'>" + result[i].title + "</a></td><td>" + result[i].id + "</td></tr>");
     }
 
 }
 
 function removeTables() {
     var tables = document.getElementsByTagName("TABLE");
-    for (var i=tables.length-1; i>=0;i-=1)
+    for (var i = tables.length - 1; i >= 0; i -= 1)
         if (tables[i]) tables[i].parentNode.removeChild(tables[i]);
 }
-
-/*
-function getSong() {
-    var song = document.getElementById("inputSong").value;
-    console.log(song);
-    var url = "http://www.songsterr.com/a/ra/songs/bysongs.json?";
-    url += $.param({
-        'songs': song
-    });
-    console.log (url);
-    $.ajax({
-        //    url: "http://www.songsterr.com/a/ra/songs/byartists.json?artists=",
-        url: url,
-        type: 'GET',
-        crossDomain: true,
-        dataType: 'json',
-        success: function(result) { console.log(result); },
-        error: function() { alert('Failed!'); }
-    });
-
-}
-    */
